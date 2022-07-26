59426986a78e3baa1eed0f6243f033125b8e2eb2
Jose C Morgado Vargas <josecruzmv@outlook.com>
2022-07-25 19:39:26 -0500

    Main functions of the player working directly in viewmodel now

--- app/src/main/java/com/example/musicplayer/ui/view/SongsPlayerFragment.kt
+++ app/src/main/java/com/example/musicplayer/ui/view/SongsPlayerFragment.kt
@@ -8,18 +8,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.SeekBar
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.example.musicplayer.R
 import com.example.musicplayer.core.MediaPlayerHelper
 import com.example.musicplayer.core.QueueHelper
 import com.example.musicplayer.databinding.SongsPlayerFragmentBinding
 import com.example.musicplayer.domain.model.AudioModel
+import com.example.musicplayer.ui.viewmodel.SongsPlayerViewModel
 import java.io.IOException
 
 class SongsPlayerFragment : Fragment() {
     private lateinit var binding: SongsPlayerFragmentBinding
-    private val mediaPlayer = MediaPlayerHelper.getInstance()
-    private lateinit var currentSong: AudioModel
+
+    private val songsPlayerViewModel: SongsPlayerViewModel by viewModels ()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,33 +34,50 @@ class SongsPlayerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
-            ivNext.setOnClickListener { playNextSong() }
-            ivPrevious.setOnClickListener { playPreviousSong() }
-            ivPausePlay.setOnClickListener { pausePlay() }
+
+            ivNext.setOnClickListener { songsPlayerViewModel.nextSong() }
+            ivPrevious.setOnClickListener { songsPlayerViewModel.previousSong() }
+            ivPausePlay.setOnClickListener { songsPlayerViewModel.pausePlay() }
 
             sbProgress.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
                 override fun onProgressChanged(p0: SeekBar?, progress: Int, fromUser: Boolean) {
-                    if (mediaPlayer.isPlaying && fromUser)
-                        mediaPlayer.seekTo(progress)
+                    if (fromUser)
+                        songsPlayerViewModel.seekSongTo(progress)
                 }
-
                 override fun onStartTrackingTouch(p0: SeekBar?) {}
-
                 override fun onStopTrackingTouch(p0: SeekBar?) {}
-
             })
         }
-        setResourcesWithMusic()
+        songsPlayerViewModel.getCurrentSong()
 
+        songsPlayerViewModel.currentSong.observe(viewLifecycleOwner) { currentSong ->
+            binding.apply {
+                tvPlayerTitle.text = currentSong.title
+                sbProgress.progress = 0
+                sbProgress.max = currentSong.duration.toInt()
+                tvTotalTime.text = convertMilliToMMSS(currentSong.duration)
+                ivPausePlay.setImageResource(R.drawable.ic_pause)
+            }
+        }
 
+        songsPlayerViewModel.isSongPlaying.observe(viewLifecycleOwner) { isSongPlaying ->
+            binding.apply {
+                if (isSongPlaying)
+                    ivPausePlay.setImageResource(R.drawable.ic_pause)
+                else
+                    ivPausePlay.setImageResource(R.drawable.ic_play)
+            }
+        }
 
+        /*TODO Implement handler directly in view model */
+        /*TODO Play next song when current finishes */
         val handler = Handler(Looper.getMainLooper())
         handler.postDelayed(object : Runnable {
             override fun run() {
                 try {
                     binding.apply {
-                        sbProgress.progress = mediaPlayer.currentPosition
-                        tvCurrentTime.text = convertMilliToMMSS(mediaPlayer.currentPosition.toString())
+                        sbProgress.progress = songsPlayerViewModel.getSongProgression()
+                        tvCurrentTime.text = convertMilliToMMSS(songsPlayerViewModel.getSongProgression().toString())
 
                     }
                     handler.postDelayed(this, 1000)
@@ -68,64 +87,6 @@ class SongsPlayerFragment : Fragment() {
             }
         }, 0)
 
-    }
-
-    private fun setResourcesWithMusic() {
-        //currentSong = MediaPlayerHelper.songsList[MediaPlayerHelper.currentIndex]
-        currentSong = QueueHelper.songsList[QueueHelper.currentIndex]
-        binding.apply {
-            tvPlayerTitle.text = currentSong.title
-            tvTotalTime.text = convertMilliToMMSS(currentSong.duration)
-            ivPausePlay.setImageResource(R.drawable.ic_pause)
-        }
-        playMusic()
-    }
-
-    private fun playMusic() {
-        mediaPlayer.reset()
-
-        try {
-            mediaPlayer.setDataSource(currentSong.path)
-            mediaPlayer.prepare()
-            mediaPlayer.start()
-            binding.apply {
-                sbProgress.progress = 0
-                sbProgress.max = mediaPlayer.duration
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-    }
-
-    private fun playNextSong() {
-        if (MediaPlayerHelper.currentIndex == MediaPlayerHelper.songsList.lastIndex) {
-            MediaPlayerHelper.currentIndex = 0
-        } else {
-            MediaPlayerHelper.currentIndex += 1
-        }
-        mediaPlayer.reset()
-        setResourcesWithMusic()
-    }
-
-    private fun playPreviousSong() {
-        if (MediaPlayerHelper.currentIndex == 0) {
-            MediaPlayerHelper.currentIndex = MediaPlayerHelper.songsList.lastIndex
-        } else {
-            MediaPlayerHelper.currentIndex -= 1
-        }
-        mediaPlayer.reset()
-        setResourcesWithMusic()
-    }
-
-    private fun pausePlay() {
-        if (mediaPlayer.isPlaying) {
-            mediaPlayer.pause()
-            binding.ivPausePlay.setImageResource(R.drawable.ic_play)
-        } else {
-            mediaPlayer.start()
-            binding.ivPausePlay.setImageResource(R.drawable.ic_pause)
-
-        }
     }
 
     private fun convertMilliToMMSS(duration: String): String {
---
+++ app/src/main/java/com/example/musicplayer/ui/viewmodel/SongsPlayerViewModel.kt
@@ -0,0 +1,78 @@
+package com.example.musicplayer.ui.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.*
+import com.example.musicplayer.core.MediaPlayerHelper
+import com.example.musicplayer.core.MediaPlayerHelper.mediaPlayer
+import com.example.musicplayer.core.QueueHelper
+import com.example.musicplayer.domain.model.AudioModel
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+
+class SongsPlayerViewModel : ViewModel() {
+
+    val currentSong = MutableLiveData<AudioModel>()
+    lateinit var _currentSong: AudioModel
+    val isSongPlaying = MutableLiveData<Boolean>()
+
+
+    private val mediaPlayer = MediaPlayerHelper.getInstance()
+
+    fun getCurrentSong() {
+        _currentSong = QueueHelper.songsList[QueueHelper.currentIndex]
+        currentSong.postValue(_currentSong)
+        playMusic()
+
+        CoroutineScope(IO).launch {
+            launch { getSongProgression() }
+            delay(1000)
+        }
+    }
+
+    fun playMusic() {
+        mediaPlayer.reset()
+
+        try {
+            mediaPlayer.setDataSource(_currentSong.path)
+            mediaPlayer.prepare()
+            mediaPlayer.start()
+
+        } catch (e: Exception) {
+            Log.d("Player", e.toString())
+        }
+    }
+
+    fun nextSong() {
+        if (QueueHelper.currentIndex == QueueHelper.songsList.lastIndex) {
+            QueueHelper.currentIndex = 0
+        } else {
+            QueueHelper.currentIndex += 1
+        }
+        mediaPlayer.reset()
+        getCurrentSong()
+    }
+
+    fun previousSong() {
+        if (QueueHelper.currentIndex == 0) {
+            QueueHelper.currentIndex = QueueHelper.songsList.lastIndex
+        } else {
+            QueueHelper.currentIndex -= 1
+        }
+        mediaPlayer.reset()
+        getCurrentSong()
+    }
+
+    fun pausePlay() {
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.pause()
+            isSongPlaying.postValue(false)
+        } else {
+            mediaPlayer.start()
+            isSongPlaying.postValue(true)
+        }
+    }
+
+    fun getSongProgression() = mediaPlayer.currentPosition
+
+    fun seekSongTo(moveTo: Int) = mediaPlayer.seekTo(moveTo)
+}
