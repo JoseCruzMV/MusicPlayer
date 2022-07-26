93b7d41be9fbbac7895b615b010b639b6d7dad99
Jose C Morgado Vargas <josecruzmv@outlook.com>
2022-07-26 17:57:30 -0500

    Added. Now main roster show which song is playing

--- app/src/main/java/com/example/musicplayer/core/MediaPlayerHelper.kt
+++ app/src/main/java/com/example/musicplayer/core/MediaPlayerHelper.kt
@@ -1,11 +1,8 @@
 package com.example.musicplayer.core
 
 import android.media.MediaPlayer
-import com.example.musicplayer.domain.model.AudioModel
 
 object MediaPlayerHelper {
-    var currentIndex = -1
-    var songsList = emptyList<AudioModel>()
-    val mediaPlayer = MediaPlayer()
+    private val mediaPlayer = MediaPlayer()
     fun getInstance() = mediaPlayer
 }
--- app/src/main/java/com/example/musicplayer/ui/view/rostersongs/RosterSongsAdapter.kt
+++ app/src/main/java/com/example/musicplayer/ui/view/rostersongs/RosterSongsAdapter.kt
@@ -1,11 +1,13 @@
 package com.example.musicplayer.ui.view.rostersongs
 
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.example.musicplayer.core.MediaPlayerHelper
+import com.example.musicplayer.R
 import com.example.musicplayer.core.QueueHelper
 import com.example.musicplayer.databinding.RosterSongsItemBinding
 import com.example.musicplayer.domain.model.AudioModel
@@ -31,24 +33,23 @@ class RosterRowHolder(
     private val binding: RosterSongsItemBinding,
     private val onClick: () -> Unit,
 ) : RecyclerView.ViewHolder(binding.root) {
-    fun bind(song: AudioModel) =
-        binding.apply {
-            tvSongTitle.text = song.title
+    fun bind(song: AudioModel) = binding.apply {
+        val color = ResourcesCompat.getColor(Resources.getSystem(), R.color.secondary, null)
+        if (layoutPosition == QueueHelper.currentIndex)
+            tvSongTitle.setTextColor(color)
+        tvSongTitle.text = song.title
 
-            clItemContainer.setOnClickListener {
-                MediaPlayerHelper.getInstance().reset()
-                MediaPlayerHelper.currentIndex = layoutPosition
-                QueueHelper.currentIndex = layoutPosition
-                onClick()
-            }
+        clItemContainer.setOnClickListener {
+            QueueHelper.currentIndex = layoutPosition
+            onClick()
         }
+    }
 }
 
 private object DiffCallBack : DiffUtil.ItemCallback<AudioModel>() {
     override fun areItemsTheSame(oldItem: AudioModel, newItem: AudioModel) =
         oldItem.path == newItem.path
 
-    override fun areContentsTheSame(oldItem: AudioModel, newItem: AudioModel)=
+    override fun areContentsTheSame(oldItem: AudioModel, newItem: AudioModel) =
         oldItem.title == newItem.title
-
 }
